dc4770e56ecf0915dceffda52f6703131ffd28c6
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-12 06:40:09 +0600

    parameter properties

--- src/object.ts
+++ src/object.ts
@@ -1,22 +1,40 @@
-class Animal{
-    name:string;
-    species:string;
-    sound:string;
+// Normal system for class 
 
-    constructor (name:string, species:string, sound:string) {
-        this.name = name;
-        this.species = species;
-        this.sound = sound;
-    }
+// class Animal{
+//     name:string;
+//     species:string;
+//     sound:string;
+
+//     constructor (name:string, species:string, sound:string) {
+//         this.name = name;
+//         this.species = species;
+//         this.sound = sound;
+//     }
+
+//     makeSound() {
+//         console.log(`The ${this.name} says ${this.sound}`);
+//     }
+// }
+
+
+// const dog = new Animal("Triger", "Dog", "Ghew Ghew");
+// const cat = new Animal("Lovely", "Cat", "Mew mew" );
+
+// dog.makeSound();
+// cat.makeSound();
+
+
+// Parameter properties
+
+class Animal{
+    constructor(public name:string, public species:string, public sound:string){};
 
     makeSound() {
         console.log(`The ${this.name} says ${this.sound}`);
     }
 }
 
-
-const dog = new Animal("Triger", "Dog", "Ghew Ghew");
-const cat = new Animal("Lovely", "Cat", "Mew mew" );
+const dog = new Animal("Tiger dog", "Dog", "Ghew ghew");
 
 dog.makeSound();
-cat.makeSound();
+
