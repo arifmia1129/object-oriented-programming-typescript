9c042b20c0d9f3572bc79c93ca9bf39209f96f72
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-12 07:26:18 +0600

    keyof guard

---
+++ src/type-guard.ts
@@ -0,0 +1,10 @@
+// keyof guard
+type Alphanumeric = string | number;
+
+function add(param1:Alphanumeric, param2:Alphanumeric):Alphanumeric{
+    if(typeof(param1)=== "number" && typeof(param2) === "number"){
+        return param1 + param2;
+    }else{
+        return param1.toString() + param2.toString();
+    }
+}
