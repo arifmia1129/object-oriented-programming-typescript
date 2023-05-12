17abb2495a276208e682b30d026f74d26e9ed342
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-12 07:39:45 +0600

    in guard

--- src/type-guard.ts
+++ src/type-guard.ts
@@ -8,3 +8,28 @@ function add(param1:Alphanumeric, param2:Alphanumeric):Alphanumeric{
         return param1.toString() + param2.toString();
     }
 }
+
+// in guard
+
+type NormalUser = {
+    name:string
+}
+
+type AdminUser = {
+    name:string,
+    role:"admin"
+}
+
+const normalUser1:NormalUser= {name:"Arif"};
+const adminUser1:AdminUser = {name:"Binu", role:"admin"};
+
+function checkUser(user:NormalUser|AdminUser):string{
+    if("role" in user){
+        return `${user?.name} is an admin`;
+    }else{
+        return `${user.name} is an user`;
+    }
+}
+
+console.log(checkUser(normalUser1));
+console.log(checkUser(adminUser1));
